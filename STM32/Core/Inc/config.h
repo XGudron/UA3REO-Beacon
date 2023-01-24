196f00370f8258f4342789e76283b876014b5a13
dmitry.panin <d.panin@skyeng.ru>
2023-01-24 18:54:06 +0300

    Add 10ghz and long dash

--- STM32/Core/Inc/config.h
+++ STM32/Core/Inc/config.h
@@ -7,9 +7,12 @@
 #define POWER_LEVEL 4 // 1 (-4 dBm), 2 (-1 dBm), 3 (+2 dBm), 4(+5 dBm)
 #define REFERENCE_CLOCK 10000000 // 25000000 default XO, 10000000 OCXO
 
-//static uint64_t FREQUENCIES[] = {24930, 28200, 144445500, 432450000, 1296900000, 5760895000}; // ??? 5184435000 x2 -> 10368870000 ; 3456290000 x3 -> 10368870000
-static uint64_t FREQUENCIES[] = {144445500, 432450000, 1296900000, 5760895000};
-//static uint64_t FREQUENCIES[] = {144445500};
+#define LONG_DASH_BEFORE_MESSAGE true // true/false long signal (3sec) before start transmit message
+
+//static uint64_t FREQUENCIES[] = {24930, 28200, 144445500, 432450000, 1296900000, 5760895000, 3456290000}; // 12m, 10m, 2m, 70cm, 23cm, 6cm, 3cm (10ghz band from 3x harmonics)
+static uint64_t FREQUENCIES[] = {144445500, 432450000, 1296900000}; // 2m, 70cm, 23cm
+//static uint64_t FREQUENCIES[] = {144445500, 432450000, 1296900000, 5760895000, 3456290000}; // 2m, 70cm, 23cm, 6cm, 3cm (10ghz band from 3x harmonics)
+//static uint64_t FREQUENCIES[] = {3456290000}; // single band
 static char CW_MESSAGE[] = "CQ CQ BEACON 73";
 
 #endif
--- STM32/Core/Src/transmitter.c
+++ STM32/Core/Src/transmitter.c
@@ -20,6 +20,27 @@ void TRANSMITTER_Process(void) {
 		}
 		
 		MAX2870_Init(FREQUENCIES[TRANSMITTER_Band_index]);
+		
+		// LONG_DASH
+		if (LONG_DASH_BEFORE_MESSAGE) {
+			uint32_t startTime = HAL_GetTick();
+			while((HAL_GetTick() - startTime) < 1000) {
+				HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(MAX_CE_GPIO_Port, MAX_CE_Pin, GPIO_PIN_RESET);
+			}
+			
+			startTime = HAL_GetTick();
+			while((HAL_GetTick() - startTime) < 3000) {
+				HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(MAX_CE_GPIO_Port, MAX_CE_Pin, GPIO_PIN_SET);
+			}
+			
+			startTime = HAL_GetTick();
+			while((HAL_GetTick() - startTime) < 1000) {
+				HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(MAX_CE_GPIO_Port, MAX_CE_Pin, GPIO_PIN_RESET);
+			}
+		}
 	}
 	
 	HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, CW_TransmitStatus ? GPIO_PIN_RESET : GPIO_PIN_SET);
